ee93cdbba4f9e823a9d6c3c9eb5cd6f8c7d2a2c5
WheresAlice <WheresAlice@users.noreply.github.com>
2018-06-16 15:20:37 +0100

    add markdown linting

--- src/components/editor.js
+++ src/components/editor.js
@@ -1,14 +1,19 @@
 import React from "react"
 import Markdown from "markdown"
+import Axios from "axios"
 
 class Editor extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {message: ''}
+    this.state = {output: '', lint: [], input: ''}
   }
   onChange(e) {
     let output = Markdown.parse(e.currentTarget.value)
-    this.setState({message: output});
+    this.setState({output: output});
+    this.setState({input: e.currentTarget.value})
+  }
+  mdl(e) {
+    Axios.post('https://immense-coast-50534.herokuapp.com/', this.state.input).then(response => this.setState({lint: response.data}));
   }
   render() {
     const wrapperStyle = {
@@ -24,10 +29,26 @@ class Editor extends React.Component {
     const displayStyle = {
       gridColumn: 2
     }
+    const alertStyle = {
+      color: 'red'
+    }
+    const RenderLint = (props) => {
+      return(
+        <ul>
+          {props.items.map((item, i) =>
+          <li index={i} style={alertStyle}>{item}</li>
+          )}
+        </ul>
+      )
+    }
     return (
+      <div>
+        <button onClick={this.mdl.bind(this)}>Lint</button>
       <div style={wrapperStyle}>
       <textarea id="editor" onChange={this.onChange.bind(this)} rows="10" style={textareaStyle} />
-      <div id="display" dangerouslySetInnerHTML={{ __html: this.state.message }} style={displayStyle} />
+      <div id="display" dangerouslySetInnerHTML={{ __html: this.state.output }} style={displayStyle} />
+      </div>
+        <RenderLint items={this.state.lint}/>
       </div>
     )
   }
--- src/pages/editor.js
+++ src/pages/editor.js
@@ -5,7 +5,7 @@ class EditorPage extends Component {
   render() {
     return (
       <div>
-        <p>Put some Markdown in the box on the left, get some html rendered on the right.</p>
+        <p>Put some Markdown in the box on the left, get some html rendered on the right.  Press 'Lint' to get any warnings about your markdown style.</p>
         <Editor/>
         <p><a href="https://github.com/WheresAlice/markdown-editor">See the source code</a></p>
       </div>
